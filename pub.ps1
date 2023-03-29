1442603fe49d259d47749205c15ad90ab6aba061
KevinT3Hu <kevint3hu@outlook.com>
2023-03-29 21:34:54 +0800

    Update

---
+++ pub.ps1
@@ -0,0 +1,7 @@
+bundle exec jekyll build --config _config.yml --increment
+Write-Output "Build complete"
+Set-Location _site
+git add *
+git commit -m "Update"
+git push origin main
+Write-Output "Done"
