b6a51cd3bfc519dafce5fe32322ce474f4f10a05
KevinT3Hu <kevint3hu@outlook.com>
2023-03-30 12:47:55 +0800

    fix style

--- pub.ps1
+++ pub.ps1
@@ -1,8 +1,17 @@
+# parse arguments: -m "commit message"
+$arguments = $args
+$commitMessage = "Update"
+for ($i = 0; $i -lt $arguments.Length; $i++) {
+    if ($arguments[$i] -eq "-m") {
+        $commitMessage = $arguments[$i + 1]
+    }
+}
+
 bundle exec jekyll build --config _config.yml
 Write-Output "Build complete"
 Set-Location _site
 git add *
-git commit -m "Update"
+git commit -m "$commitMessage"
 git push origin main
 Write-Output "Done"
 Set-Location ../
