9a23a2ba70bebcfec2224b47318b112dd87c90a0
KevinT3Hu <kevint3hu@outlook.com>
2023-05-28 01:05:41 +0800

    New post

--- pub.ps1
+++ pub.ps1
@@ -1,17 +1,25 @@
 # parse arguments: -m "commit message"
 $arguments = $args
 $commitMessage = "Update"
+$build = $false
 for ($i = 0; $i -lt $arguments.Length; $i++) {
     if ($arguments[$i] -eq "-m") {
         $commitMessage = $arguments[$i + 1]
     }
+    if ($arguments[$i] -eq "-b") {
+        $build = $true
+    }
+}
+if ($build) {
+    bundle exec jekyll build --config _config.yml
+    Write-Output "Build complete"
+    Set-Location _site
+    git add *
+    git commit -m "$commitMessage"
+    Set-Location ../
 }
 
-bundle exec jekyll build --config _config.yml
-Write-Output "Build complete"
 Set-Location _site
-git add *
-git commit -m "$commitMessage"
 git push origin main
 Write-Output "Done"
 Set-Location ../
